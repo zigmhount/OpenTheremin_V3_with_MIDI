69d139a14c1c0196e95f0d75f48114863f948799
MrDham <vincent.dhamelincourt@wanadoo.fr>
2026-03-21 09:12:08 +0100

    Update application.cpp
    Decreased "Parameter" potentiometer surveillance to avoid looking after non significant noise and reduce led togling from "Mute and Play status" to "Both LEDs ON/OFF setting monitoring".
    
    UI : Trigger Volume / Velocity:
    Both LEDs are off: NOTE messages generated
    Both LED are on: NOTE messages not generated:

--- Open_Theremin_V3/application.cpp
+++ Open_Theremin_V3/application.cpp
@@ -907,7 +907,7 @@ void Application::set_parameters ()
   data_pot_value = analogRead(WAVE_SELECT_POT);
 
   // If parameter pot moved
-  if (abs((int32_t)param_pot_value - (int32_t)old_param_pot_value) >= 8)
+  if (abs((int32_t)param_pot_value - (int32_t)old_param_pot_value) >= 32)
   {
     // Blink the LED relatively to pot position
     resetTimer();
@@ -1028,7 +1028,14 @@ void Application::set_parameters ()
       
     case 5:
       // Volume trigger
-      data_steps = data_pot_value >> 8;
+      if ((data_pot_value >> 3) < 127)
+      {
+        data_steps = 0; // Note messages generated
+      }
+      else
+      {
+        data_steps = 1; // Note messages not generated
+      }
       midi_volume_trigger = (uint8_t)((data_pot_value >> 3) & 0x007F);
       break;
       
